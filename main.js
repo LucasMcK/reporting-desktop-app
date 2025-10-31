67f12ab5aae18468f7861f251483a7ea13a05ef6
Lucasmck14 <lucasmck14@gmail.com>
2025-10-31 12:01:25 -0600

    Added dev auto update functionality

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
 require('dotenv').config();
-require('update-electron-app')()
-const path = require('node:path')
+const { updateElectronApp } = require('update-electron-app');
+updateElectronApp();
 
-const createWindow = () => {
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
-  createWindow()
-})
+  ipcMain.handle('ping', () => 'pong');
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  if (process.platform !== 'darwin') app.quit();
+});
